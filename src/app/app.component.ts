7c513cd783c188d42a017d697b48f913fcedd200
Fe14080 <lipe.cintran1408@gmail.com>
2022-04-19 00:08:55 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  lista: string[] = ['carlos', 'joao', 'maria', 'bruno', 'pedro', 'antonio'];
+  lista: string[] = ['carlos', 'joao', 'maria', 'bruno', 'pedro'];
+
+  textoqualquer: string = 'Um texto qualquer';
+
+  data: Date = new Date();
+
+  valor: number = 12.13;
+
+  urlimage: string =
+    'https://static8.depositphotos.com/1003924/886/i/600/depositphotos_8868243-stock-photo-spectrum-multicolored-eye-macro.jpg';
+
+  isSpecial: boolean = true;
+
+  onKeyUp(event) {
+    console.log(event.target.value);
+  }
+
+  getValor(): number {
+    return 5;
+  }
+
+  getclassname(): string {
+    return 'alert-warning';
+  }
 }
